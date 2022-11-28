ea32f616191c83eb15424870ba09a0dd406ccb86
FLorine-R28 <rodriguesflorine88pgmail.com>
2022-11-28 11:31:59 +0100

    turn on

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
+function switchLight(){
+
+  const lightImg = document.getElementById('lightImg');
+  const toggleBtn = document.getElementById('toggleBtn');
+  const btnStandBy = document.getElementById('toggleBtnLabel');
+
+  if(toggleBtn.src.match("./resources/light-off.png")== -1){
+      lightImg.src = "./resources/light-off.png";
+      btnStandBy.innerHTML = "Turn off";
+  } else {
+      lightImg.src = "./resources/light-on.png";
+      btnStandBy.innerHTML = "Turn on";
+  }
+};
+
 const init = () => {
-  // Your code here ...
-}
+  toggleBtn.addEventListener('click', switchLight);
+};
 
 window.onload = init;
