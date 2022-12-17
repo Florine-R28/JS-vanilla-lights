a11eba459c303797a48ca312451a91e057ab6b7d
FLorine-R28 <rodriguesflorine88pgmail.com>
2022-12-17 11:16:07 +0100

    function switchLight

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 function switchLight(){
 
   const lightImg = document.getElementById('lightImg');
-  const toggleBtn = document.getElementById('toggleBtn');
-  const btnStandBy = document.getElementById('toggleBtnLabel');
-
-  if(toggleBtn.src.match("./resources/light-off.png")== -1){
-      lightImg.src = "./resources/light-off.png";
-      btnStandBy.innerHTML = "Turn off";
+  const toggleBtnLabel = document.getElementById('toggleBtnLabel');
+  //console.log(lightImg.src, typeof lightImg.src)
+  if(lightImg.src.match('./resources/light-off.png')){
+      lightImg.src = './resources/light-on.png';
+     
+      toggleBtnLabel.innerHTML = "Turn on";
   } else {
-      lightImg.src = "./resources/light-on.png";
-      btnStandBy.innerHTML = "Turn on";
+      lightImg.src = "./resources/light-off.png";
+      
+      toggleBtnLabel.innerHTML = 'Turn off';
   }
 };
 
 const init = () => {
+  const toggleBtn = document.getElementById('toggleBtn'); 
   toggleBtn.addEventListener('click', switchLight);
 };
 
