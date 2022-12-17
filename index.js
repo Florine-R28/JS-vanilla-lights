4381fca5672db5ed975602e2dbdaac403ad11393
FLorine-R28 <rodriguesflorine88pgmail.com>
2022-12-17 11:23:11 +0100

    optimization the code

--- index.js
+++ index.js
@@ -2,15 +2,13 @@ function switchLight(){
 
   const lightImg = document.getElementById('lightImg');
   const toggleBtnLabel = document.getElementById('toggleBtnLabel');
-  //console.log(lightImg.src, typeof lightImg.src)
-  if(lightImg.src.match('./resources/light-off.png')){
-      lightImg.src = './resources/light-on.png';
-     
-      toggleBtnLabel.innerHTML = "Turn on";
+
+  if(lightImg.src.includes('off')){
+      lightImg.src = lightImg.src.replace('off', 'on');
+      toggleBtnLabel.textContent = "Turn on";
   } else {
-      lightImg.src = "./resources/light-off.png";
-      
-      toggleBtnLabel.innerHTML = 'Turn off';
+      lightImg.src = lightImg.src.replace('on', 'off');;
+      toggleBtnLabel.textContent = 'Turn off';
   }
 };
 
